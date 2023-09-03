c06ce6f0390cd261fd8d4ff6730306ff7606138f
ajitrchv <67496650+ajitrchv@users.noreply.github.com>
2023-09-03 17:28:44 +0530

    added the files from GoForDevOps

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ajitrchv/go-jwt.git
 
-go 1.20
-
-require github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+go 1.18
--- main.go
+++ main.go
@@ -1,11 +1,47 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"github.com/ajitrchv/go-jwt.git/pkg/api"
+)
 
-func main(){
-	http.HandleFunc("/",Home)
-	http.HandleFunc("/refresh", Refresh)
-	http.HandleFunc("/login", Login)
-	http.ListenAndServe(":8080",nil)
+func main() {
+	var (
+		requestURL string
+		password   string
+		parsedURL  *url.URL
+		err        error
+	)
+	flag.StringVar(&requestURL, "url", "", "url to access")
+	flag.StringVar(&password, "password", "", "use a password to access our api")
+
+	flag.Parse()
+
+	if parsedURL, err = url.ParseRequestURI(requestURL); err != nil {
+		fmt.Printf("Help: ./http-get -h\nURL is not valid URL: %s\n", requestURL)
+		os.Exit(1)
+	}
+
+	apiInstance := api.New(api.Options{
+		Password: password,
+		LoginURL: parsedURL.Scheme + "://" + parsedURL.Host + "/login",
+	})
+
+	res, err := apiInstance.DoGetRequest(parsedURL.String())
+	if err != nil {
+		if requestErr, ok := err.(api.RequestError); ok {
+			fmt.Printf("Error occurred: %s (HTTP Error: %d, Body: %s)\n", requestErr.Error(), requestErr.HTTPCode, requestErr.Body)
+			os.Exit(1)
+		}
+		fmt.Printf("Error occurred: %s\n", err)
+		os.Exit(1)
+	}
+	if res == nil {
+		fmt.Printf("No response\n")
+		os.Exit(1)
+	}
+	fmt.Printf("Response: %s\n", res.GetResponse())
 }
-
